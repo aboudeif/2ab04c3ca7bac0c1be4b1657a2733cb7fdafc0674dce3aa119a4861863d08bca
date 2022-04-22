2778b626435aa895fed2b08676ca085598eeec63
Abdallah Aboudeif <comofcom@gmail.com>
2022-04-22 08:24:35 +0200

    add allposts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,33 +1,55 @@
 <head>
-    <link rel="stylesheet" href="css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-<table>
+    {{-- nav --}}
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link" href="#">Features</a>
+              <a class="nav-link" href="#">Pricing</a>
+              <a class="nav-link disabled">Disabled</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    {{-- /nav --}}
+    <div class="container mt-5">
+    {{-- table --}}
+    <a href="#" class="btn btn-success mb-3">Create post</a>
+<table class="table">
     <thead>
         <tr>
-            <th>id</th>
-            <th>title</th>
-            <th>posted by</th>
-            <th>posted at</th>
-            <th>actions</th>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">Posted By</th>
+            <th scope="col">Posted At</th>
+            <th scope="col">Actions</th>
         </tr>
     </thead>
-
 @foreach ($allPosts as $post)
     <tbody>
-        <tr>
+        <tr scope="row">
             <th>{{ $post['id'] }}</th>
             <th>{{ $post['title'] }}</th>
             <th>{{ $post['posted_by'] }}</th>
             <th>{{ $post['posted_at'] }}</th>
             <th>
-                <button>Edit</button>
-                <button>Publish</button>
-                <button>Delete</button>
+                <a href="#" class="btn btn-info">View</a>
+                <a href="#" class="btn btn-primary">Edit</a>
+                <a href="#" class="btn btn-danger">Delete</a>
             </th>
         </tr>
     </tbody>
 @endforeach 
-
 </table>
+{{-- table --}}
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
